bfb7ff76179c08de7aba0b6a4cabeebb71e35b89
yasu <yasu@MacBookAir.local>
2014-12-19 12:36:29 +0900

    Initial Commit

---
+++ SampleCustomFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCustomFont
+//
+//  Created by yasu on 2014/12/19.
+//  Copyright (c) 2014年 ho9ho9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
